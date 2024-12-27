4cc7b21ef5511678c6f3490af91e96e86169dd4f
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-27 19:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let elem=document.getElementById("level");
+let count=1;
+if(elem.parentElement){
+	count++;
+	elem=elem.parentElement;
+}
+console.log('The level of the element is: ${count}')
